d9391ab3efadbacc27b51f6aa561fa4224a50c14
Julio Quevedo <juliovquevedo@gmail.com>
2018-07-12 16:53:26 -0400

    changed title to appear on setTimeout

--- poem.js
+++ poem.js
@@ -1,13 +1,22 @@
 window.onload = function () {
+    var titleLines = setTimeout (showTitle, 1000);
+    var line = 0
+    
+    function showTitle () {
+        document.getElementsByClassName("show")[line].style.color = "black";
+        document.getElementsByClassName("show")[line++].style.transition = "color 1s linear 0.1s";
+        document.getElementsByClassName("show")[line].style.color = "black";
+        document.getElementsByClassName("show")[line++].style.transition = "color 1s linear 0.1s";
+    }
     var appear = setInterval(showLine, 3500);
     var line = 0;
     
     function showLine () {
         if (line == 0){
             document.getElementsByClassName("show")[line].style.color = "black";
-            document.getElementsByClassName("show")[line++].style.transition = "color 0.4s linear 0.1s";
+            
             document.getElementsByClassName("show")[line].style.color = "black";
-            document.getElementsByClassName("show")[line++].style.transition = "color 0.4s linear 0.1s";
+            document.getElementsByClassName("show")[line++].style.transition = "color 0.8s linear 0.1s";
         }
         else{
             document.getElementsByClassName("show")[line].style.color = "black";
