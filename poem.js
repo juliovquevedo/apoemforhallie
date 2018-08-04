b019e972f67ee6bcf1638b7228b9eaffc0a5635e
Julio Quevedo <juliovquevedo@gmail.com>
2018-08-03 23:00:02 -0400

    changed timing of title, body and signature

--- poem.js
+++ poem.js
@@ -1,29 +1,39 @@
 window.onload = function () {
-    var titleLines = setTimeout (showTitle, 1000);
-    var line = 0
+    var line = 0;
+    
+    
+    
+    
+    var stopper = setTimeout (showTitle(), 1000);
     
     function showTitle () {
         document.getElementsByClassName("show")[line].style.color = "black";
-        document.getElementsByClassName("show")[line++].style.transition = "color 1s linear 0.1s";
+        document.getElementsByClassName("show")[line++].style.transition = "color 1s linear 1s";
         document.getElementsByClassName("show")[line].style.color = "black";
-        document.getElementsByClassName("show")[line++].style.transition = "color 1s linear 0.1s";
+        document.getElementsByClassName("show")[line++].style.transition = "color 1s linear 1s";
+        clearTimeout (stopper);
     }
-    var appear = setInterval(showLine, 2800);
-    var line = 0;
+    
+    
+    
+    
+    var appear = setInterval(showLine, 0);
     
     function showLine () {
-        if (line == 0){
+        document.getElementsByClassName("show")[line].style.color = "black";
+        document.getElementsByClassName("show")[line++].style.transition = "color 1.5s linear 5s";
+        
+        if (line > 13){
             document.getElementsByClassName("show")[line].style.color = "black";
-            
-            document.getElementsByClassName("show")[line].style.color = "black";
-            document.getElementsByClassName("show")[line++].style.transition = "color 0.8s linear 0.1s";
+            document.getElementsByClassName("show")[line++].style.transition = "color 1.5s linear 30s";
         }
-        else{
+        
+        if (line == 15){
             document.getElementsByClassName("show")[line].style.color = "black";
-            document.getElementsByClassName("show")[line++].style.transition = "color 0.5s linear 0.1s";
-        }
-        if (line > 14){
+            document.getElementsByClassName("show")[line++].style.transition = "color 1.5s linear 33s";
             window.clearInterval(appear);
         }
     }
+    
+    
 }
